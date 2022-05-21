0eca7f0687fb950ddedfbd44638439a6a7f80dbd
guyzsarun <sarun.nunt@gmail.com>
2022-05-21 22:19:37 +0700

    Update Ansible

---
+++ ansible/roles/common/files/create_concourse.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+FILE_NAME=$1
+DOCKER_USERNAME=$2
+DOCKER_PASSWORD=$3
+
+TEAM_NAME="poc-k8s"
+
+
+# Read Github Username from file
+while read GITHUB_USER; do
+    
+    echo "Creating Pipeline for $GITHUB_USER"
+    fly -t $TEAM_NAME set-pipeline --pipeline $GITHUB_USER --config ./pipeline.yaml --var "docker.username=${GITHUB_USER}" --var "docker.password=${DOCKER_PASSWORD}" -n
+    fly -t $TEAM_NAME expose-pipeline --pipeline $GITHUB_USER
+    fly -t $TEAM_NAME unpause-pipeline -p $GITHUB_USER
+    
+    # fly -t $TEAM_NAME destroy-pipeline -p $GITHUB_USER -n
+    
+done < $FILE_NAME
