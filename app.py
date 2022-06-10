df13c042603f7f4f141871e921f7af38f2e8bf8f
guyzsarun <sarun.nunt@gmail.com>
2022-06-10 13:14:39 +0700

    Fix app

--- app.py
+++ app.py
@@ -4,12 +4,11 @@ import json
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    with open('data/products.json') as f:
-        data = json.load(f)
+    return render_template("index.html")
 
-    return render_template('index.html', products=data)
 
 if __name__ == "__main__":
     app.run(debug=True)
