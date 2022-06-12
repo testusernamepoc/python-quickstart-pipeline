158c359b01550e8fa2703efde8ab77326477f053
guyzsarun <sarun.nunt@gmail.com>
2022-06-12 20:34:29 +0700

    Update route

--- app.py
+++ app.py
@@ -10,5 +10,10 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/square/<int:number>")
+def calculate(number):
+    return "{} squared is {}".format(number, number**2)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
